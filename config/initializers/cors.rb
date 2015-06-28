69cda88030767f8fcb29ca49c04ae6ee730a62df
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-06-28 20:54:31 +0200

    update cors.rb to allow spamgun firebase app to access api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
  Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
    allow do
-     origins 'localhost:3001', 'localhost:3000', 'https://parrotdash.firebaseapp.com'
+     origins 'localhost:3001', 'localhost:3000', 'https://spamgun.firebaseapp.com'
 
      resource '*',
        headers: :any,
