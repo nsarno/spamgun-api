0bc2728d8ce4084a2ffb390419d5d0f5d97e60d3
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-07-07 19:24:49 +0200

    add test for automatic hostname update before ad save

--- test/models/ad_test.rb
+++ test/models/ad_test.rb
@@ -12,19 +12,22 @@ class AdTest < ActiveSupport::TestCase
   end
 
   test 'validates presence of uid' do
-    ad = ads(:one)
-
     ad_without_uid = Ad.new
     assert_not ad_without_uid.valid?
     assert ad_without_uid.errors.messages[:uid].any?
   end
 
   test 'validates presence of source' do
-    ad = ads(:one)
-
     ad_without_source = Ad.new uid: 42
 
     assert_not ad_without_source.valid?
     assert ad_without_source.errors.messages[:source_id].any?
+  end
+
+  test 'update hostname attribute automatically' do
+    source = sources(:one)
+    ad = Ad.create uid: 42, source: source
+
+    assert_equal source.hostname, ad.hostname
   end
 end
